d0fd26077a89a3e83611212f500a239de629187f
KhanhNT0710 <trongkhanh0710@gmail.com>
2024-10-26 01:45:44 +0700

    up

--- server.js
+++
@@ -1,9 +0,0 @@
-const { createServer } = require('json-server');
-
-const server = createServer();
-const router = require('json-server').router('db.json');
-const middlewares = require('json-server').defaults();
-
-server.use(middlewares);
-server.use(router);
-server.listen(process.env.PORT || 3000);
