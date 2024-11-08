8aaa10489092fa47fff0e5436cd0b6d3829bbeae
KhanhNT0710 <trongkhanh0710@gmail.com>
2024-11-08 23:38:23 +0700

    up

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -66,6 +66,22 @@ const FooterComponent = () => {
               </a>
             </li>
             <li>
+              <a href="https://maps.app.goo.gl/BSec4jZepiQVDknx9">
+                <svg
+                  xmlns="https://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-geo-alt"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
+                  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                </svg>
+                <span>Chỉ đường</span>
+              </a>
+            </li>
+            <li>
               <a href="https://zalo.me/0988015093">
                 <svg
                   xmlns="https://www.w3.org/2000/svg"
@@ -98,22 +114,7 @@ const FooterComponent = () => {
                 <span>ZaLo</span>
               </a>
             </li>
-            <li>
-              <a href="https://maps.app.goo.gl/BSec4jZepiQVDknx9">
-                <svg
-                  xmlns="https://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-geo-alt"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
-                  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-                </svg>
-                <span>Chỉ đường</span>
-              </a>
-            </li>
+
           </ul>
         </div>
         <Col className="footer-item gap-3" xs={24} sm={12} md={12}>
@@ -129,7 +130,7 @@ const FooterComponent = () => {
             </p>
             <p className="footer-item__description">
               <a target="_blank" href="https://zalo.me/0988015093">
-                Điện thoại: +8488015093
+                Điện thoại: +84 88015093
               </a>
             </p>
             <p className="footer-item__description">
@@ -149,11 +150,11 @@ const FooterComponent = () => {
               </a>
             </p>
           </div>
-          <div className="footer-item__bottom">
+          {/* <div className="footer-item__bottom">
             <FacebookOutlined />
             <InstagramOutlined />
             <TwitterOutlined />
-          </div>
+          </div> */}
           <div className=" footer-item__bottom">
             <p>
               {" "}
--- src/pages/Travel/index.jsx
+++ src/pages/Travel/index.jsx
@@ -12,7 +12,7 @@ const TravelPage = () => {
   return (
     <div className="about-container">
       <h3>Tour Trải Nghiệm Học Làm Đèn Lồng Hội An tại Thiên Đăng</h3>
-      <img src={`${API_URL}/den/slide/5.png`} alt="" />
+      <img src={`${API_URL}/den/Xuong-san-xuat/2.png`} alt="" />
       <p>
         Thiên Đăng hân hạnh giới thiệu tour trải nghiệm đặc biệt, nơi du khách
         không chỉ tham quan mà còn trực tiếp tham gia vào nghệ thuật làm đèn
@@ -20,19 +20,19 @@ const TravelPage = () => {
       </p>
       <h3> Nội Dung Tour:</h3>
       <h5> 1. Học Dán Đèn Lồng</h5>
-      <img src={`${API_URL}/den/slide/6.png`} alt="" />
+      <img src={`${API_URL}/den/Xuong-san-xuat/5.png`} alt="" />
       <p>
         Khám phá không gian trang trí đèn lồng rực rỡ, lý tưởng để lưu giữ những
         khoảnh khắc đẹp trong khung cảnh mang đậm nét văn hóa Việt.
       </p>
       <h5> 3. Thưởng Thức Ẩm Thực Văn Hóa</h5>
-      <img src={`${API_URL}/den/slide/6.png`} alt="" />
+      <img src={`${API_URL}/den/Xuong-san-xuat/4.png`} alt="" />
       <p>
         Ngoài hoạt động trải nghiệm, du khách còn có cơ hội thưởng thức các món
         ăn địa phương đặc trưng, tăng thêm phần phong phú cho chuyến đi.
       </p>
       <h3> Lợi Ích Cho Khách Hàng</h3>
-      <img src={`${API_URL}/den/slide/6.png`} alt="" />
+      <img src={`${API_URL}/den/Xuong-san-xuat/1.png`} alt="" />
       <p>
         • Trải Nghiệm Thực Tế: Hiểu thêm về quy trình thủ công truyền thống của
         người dân Hội An, từ đó cảm nhận sâu sắc văn hóa địa phương.
@@ -45,7 +45,7 @@ const TravelPage = () => {
         • Khoảnh Khắc Đáng Nhớ: Không gian đèn lồng rực rỡ và các món ăn địa
         phương giúp lưu giữ những trải nghiệm khó quên về Hội An.
       </p>
-      <img src={`${API_URL}/den/slide/6.png`} alt="" />
+      <img src={`${API_URL}/den/Xuong-san-xuat/3.png`} alt="" />
       <p>
         Tour trải nghiệm này là lựa chọn hoàn hảo để du khách khám phá và kết
         nối sâu sắc với văn hóa Hội An. Hãy liên hệ ngay với chúng tôi để đặt
